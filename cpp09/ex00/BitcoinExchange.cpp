04c3f8503ec9a972d8452ecbaf1189db398f7ae4
Reddyboi <reddybulanadi@gmail.com>
2024-08-14 20:20:00 +0200

    need to do bisextile

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -53,10 +53,7 @@ void	BitcoinExchange::InputParse(std::string file, std::map <std::string, float>
 	{
 		std::getline(input, line); //skip first line
 		while (std::getline(input, line))
-		{
-			//std::cout << line << std::endl;
 			SubParse(line, datab);
-		}
 		input.close();
 	}
 	else
@@ -68,29 +65,29 @@ void	BitcoinExchange::SubParse(std::string line, std::map <std::string, float> &
 	size_t pipe = line.find('|'); 
 	if (pipe == std::string::npos) //check for pipe presence
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: bad input => " << line << RESET << std::endl;
 		return ;
 	}
 	if (line.size() < 13) //check for minimal line length
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: Invalid line 2 format" RESET << std::endl;
 		return ;
 	}
 	if (line[pipe - 1] != ' ') //check for space before pipe
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: Invalid line 3 format" RESET << std::endl;
 		return ;
 	}
 	if (line[pipe + 1] != ' ') //check for space after pipe
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: Invalid line 4 format" RESET << std::endl;
 		return ;
 	}
 	std::string sousstr = line.substr(pipe + 2, line.size()); //isolate value for more parsing boiiiiiiii
 	int i = 0;
 	if (sousstr[0] == '-') //check for negatives
 	{
-		std::cout << RED "Error: value between 0 and 1000 only" RESET << std::endl;
+		std::cout << RED "Error: not a positive number." RESET << std::endl;
 		return ;
 	}
 	if (sousstr[0] == '+') //skip '+' if present
@@ -111,28 +108,39 @@ void	BitcoinExchange::SubParse(std::string line, std::map <std::string, float> &
 	}
 	if (ft_stof(sousstr) > 1000)
 	{
-		std::cout << RED "Error: value between 0 and 1000 only" RESET << std::endl;
+		std::cout << RED "Error: too large number." RESET << std::endl;
 		return ;
 	}
-	//////////////////COMPARE/////////////////////
+	//////////////////COMPARE and CALCUL/////////////////////
 	std::string	date = line.substr(0, pipe - 1);
-	//float		value = ft_stof(sousstr);
+	float		value = ft_stof(sousstr);
 
-	std::map<std::string, float>::iterator search = datab.find(date);
-
-	if (search == datab.end())
-		std::cout << "not found" << std::endl;
-	//std::cout << date << " => " << value << " = " << value * BitcoinExchange::_second << std::endl;
+	std::map<std::string, float>::iterator it = datab.begin();
+	if (datab.find(date) == datab.end())
+	{
+		while (it++ != datab.end())
+		{
+			if (date < it->first)
+			{
+				it--;
+				std::cout << date << " => " << value << " = " << (value * it->second) << std::endl;
+				return ;
+			}
+		}
+		std::cout << "Could not find date" << std::endl;
+	}
+	std::cout << date << " => " << value << " = " << (value * datab[date]) << std::endl;
+	///////////////////////////////////////////////////////
 }
 
 void	BitcoinExchange::FillData(std::map<std::string, float> &datab)
 {
 	std::ifstream file;
 	std::string line;
-(void)datab;
 	file.open("data.csv");
 	if (file.is_open())
 	{
+		std::getline(file, line); //skip first line
 		while (std::getline(file, line))
 		{
 			DataParse(line); //PARSE DATA.CSV
@@ -149,12 +157,12 @@ void	BitcoinExchange::DataParse(std::string line)
 	size_t comma = line.find(','); 
 	if (comma == std::string::npos) //check for comma presence
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: Invalid line 1 format" RESET << std::endl;
 		return ;
 	}
 	if (line.size() < 11) //check for minimal line length
 	{
-		std::cout << RED "Error: Invalid line format" RESET << std::endl;
+		std::cout << RED "Error: Invalid line 2 format" RESET << std::endl;
 		return ;
 	}
 	std::string sousstr = line.substr(comma + 1, line.size()); //isolate exrate for more parsing boiiiiiiii
